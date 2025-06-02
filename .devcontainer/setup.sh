b2c018cce0ca845a801036b9f3fe6bfc50f88365
lai8928 <55567878+lai8928@users.noreply.github.com>
2025-06-02 05:09:14 +0000

    codespace init shcript

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "start install awscli"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+echo "finish install awscli"
