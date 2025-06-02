b36f7e9449efcd5fb2479b3d15f3d082e978a80e
lai8928 <55567878+lai8928@users.noreply.github.com>
2025-06-02 05:43:08 +0000

    update devcontainer

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -5,5 +5,7 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 sudo ./aws/install
 sudo rm awscliv2.zip
+sudo rm aws -r 
+cd CODESPACE_VSCODE_FOLDER
 
 echo "✅ finish install awscli"
